a8b4414f8e29ef02838a2c84b48bc948e6ff6c78
tom <tom@cat.com>
2019-11-26 09:45:01 +0800

    first commit

---
+++ itrip-beans/src/main/java/com/itrip/beans/pojo/Hotel.java
@@ -0,0 +1,33 @@
+package com.itrip.beans.pojo;
+
+import java.io.Serializable;
+
+public class Hotel implements Serializable {
+    private Integer id;
+    private String hotelName;
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ itrip-utils/src/main/java/com/itrip/common/ErrorCode.java
@@ -0,0 +1,6 @@
+package com.itrip.common;
+
+public class ErrorCode {
+    public  static final  String NO_ERROR="0";
+    public  static final  String DEFAULT_ERROR="-1";
+}
